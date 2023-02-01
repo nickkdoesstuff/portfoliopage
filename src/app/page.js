5dc528d6bae9b4dd761540637f05017f7057f98e
mystxii <galaxycatbro@gmail.com>
2023-02-01 14:24:31 +0000

    jg

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,6 @@
 import Image from 'next/image'
 import { Source_Code_Pro } from '@next/font/google'
 import Link from 'next/link'
-const scp = Source_Code_Pro({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -13,7 +12,7 @@ export default function Home() {
           <h1 className="font-bold text-3xl">Hi! I&apos;m Nick.</h1>
           <p>Where do you want to go first?</p>
         </div>
-        <div className={`flex flex-col gap-4 md:flex-row ${scp.className}`}>
+        <div className={`flex flex-col gap-4 md:flex-row`}>
           <Link href="/about" className="flex-grow">
             <div className='bg-zinc-700 border-2 border-zinc-600 p-4 rounded-lg hover:shadow-xl text-center'>
               <p className='font-bold text-2xl'>About Me</p>
